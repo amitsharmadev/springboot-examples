121e58fbf1bcf86f0589656aa7145dd951e0a2d3
demo <demo@example.com>
2021-04-22 12:41:05 +0530

    22nd Apr 21

--- e-service/src/main/java/com/amit/sample/UserController.java
+++ e-service/src/main/java/com/amit/sample/UserController.java
@@ -3,6 +3,8 @@ package com.amit.sample;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +15,22 @@ import com.amit.sample.domain.User;
 import com.amit.sample.repo.UserRepository;
 
 @RestController()
+@RefreshScope
 public class UserController {
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@Value("${sample.prop}")
+	String data;
+	
+	@GetMapping("/demo")
+	public String getDemo() 
+			throws InterruptedException 
+	{
+		//Thread.sleep(10000);
+		return "Data from E-Service: "+ data;
+	}
 	
 	@GetMapping("/users/")
 	public List<User> getUsers() {
